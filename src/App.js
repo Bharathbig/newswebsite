4933a3dcc7a8d50389f213c0467f71517d78ec1f
38vinay <116971195+38vinay@users.noreply.github.com>
2025-11-01 00:29:57 +0530

    for testing purpose

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header";
+// import Header from "./components/Header";
 import Home from "./components/Home";
-import News from "./components/News";
-import Sports from "./components/Sports";
-import Tech from "./components/Tech";
-import Jobs from "./components/Jobs";
-import Entertainment from "./components/entertainment";
-import Contact from "./components/contact";
-import Reporters from "./components/Reporters";
+// import News from "./components/News";
+// import Sports from "./components/Sports";
+// import Tech from "./components/Tech";
+// import Jobs from "./components/Jobs";
+// import Entertainment from "./components/Entertainment";
+// import Contact from "./components/Contact";
+// import Reporters from "./components/Reporters";
 
 
 
 const App = () => (
   <Router>
     <div className="d-flex flex-column min-vh-100">
-      <Header />
+      {/* <Header /> */}
       <main className="flex-grow-1">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/news" element={<News />} />
+          {/* <Route path="/news" element={<News />} />
           <Route path="/sports" element={<Sports />} />
           <Route path="/tech" element={<Tech />} />
           <Route path="/jobs" element={<Jobs />} />
           <Route path="/entertainment" element={<Entertainment />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/reporters" element={<Reporters />} />
+          <Route path="/reporters" element={<Reporters />} /> */}
         </Routes>
       </main>
 
