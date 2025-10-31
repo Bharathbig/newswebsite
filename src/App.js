7c1728ed897d90af043034249a6ed578e258bcfa
Devi <deviarigi@gmail.com>
2025-10-31 22:10:44 +0530

    chaned the import in the header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "../src/components/Header";
 import Home from "./components/Home";
 import News from "./components/News";
 import Sports from "./components/Sports";
