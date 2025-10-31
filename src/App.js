d7c0d36def6b6a4ac89f7f97e2e5c243b9b7babc
Devi <deviarigi@gmail.com>
2025-10-31 22:14:20 +0530

    completed the build filed issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../src/components/Header";
-import Home from "./components/Home";
-import News from "./components/News";
-import Sports from "./components/Sports";
-import Tech from "./components/Tech";
-import Jobs from "./components/Jobs";
-import Entertainment from "./components/Entertainment";
-import Contact from "./components/Contact";
-import Reporters from "./components/Reporters";
+import Home from "../src/components/Home";
+import News from "../src/components/News";
+import Sports from "../src/components/Sports";
+import Tech from "../src/components/Tech";
+import Jobs from "../src/components/Jobs";
+import Entertainment from "../src/components/Entertainment";
+import Contact from "../src/components/Contact";
+import Reporters from "../src/components/Reporters";
 const App = () => (
   <Router>
     <Header />
